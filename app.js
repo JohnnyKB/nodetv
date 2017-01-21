30ebcfcd7be24f4e873ba2f391020fb84941ccd1
Johnny Kutnowski <j.kutnob@gmail.com>
2017-01-21 13:00:45 +0100

    Added bower to the workflow

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const port = process.env.PORT || 8080;
 app.set('views', './src/views');
 app.set('view engine', 'pug');
 
+// STATIC FILES SETUP
+// app.use(express.static('public')); // in case we had a 'public' dir with src
+app.use(express.static('bower_components'));
 
 app.get('/', (req, res) => {
   res.send(`You've reached the call center`);
--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,33 @@ const gulp = require('gulp');
 const nodemon = require('gulp-nodemon');
 
 const jsFiles = ['*.js', 'src/**/*.js'];
+const views = ['src/**/*.pug'];
 
-gulp.task('run', function() {
+var files = jsFiles.concat(views);
+
+gulp.task('inject', function() {
+    var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+
+    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js']);
+    var injectOptions = {
+        ignorePath: '/public'
+    };
+
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './bower_components',
+        ignorePath: '../../bower_components'
+        // we do the above becase we set up the static folder in app.js
+    };
+
+    return gulp.src('./src/views/*.pug')
+        .pipe(wiredep(options))
+        // .pipe(inject(injectSrc, injectOptions))
+        .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('run', ['inject'], function() {
   var options = {
     script: 'app.js',
     delayTime: 1,
